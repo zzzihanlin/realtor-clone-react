4cca4b3d3459373f11bd2675c8188e0dc7765301
qIIsheng <zzzihanlin@gmail.com>
2023-03-11 14:32:46 +0800

    finished section 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import React, { Profiler } from "react";
+import ReactDOM from "react-dom/client";
+
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+// pages
+import Home from './pages/Home'
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Header from "./components/Header";
+
+
+
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Header />}>
+    <Route index element={<Home />}></Route>
+    <Route path="/profile" element={<Profile />}></Route>
+    <Route path="/sign-in" element={<SignIn />}></Route>
+    <Route path="/sign-up" element={<SignUp />}></Route>
+    <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+    <Route path="/offers" element={<Offers />}></Route>
+  </Route>
+))
+
 function App() {
   return (
-    <>
-      <h1 className='text-2xl bg-red-400'>hello world!</h1>
-    </>
+    <RouterProvider router={router} />
   );
 }
 
